1f6ba79b48dc4ace2460f1d8d41ae8f8e95a5076
nonyx777 <nahomretebo@gmail.com>
2025-05-03 23:25:33 +0300

    fixed brdf to satisfy the 3 requirements

--- BlinnPhongBRDF/blinn_phong_brdf.gd
+++ BlinnPhongBRDF/blinn_phong_brdf.gd
@@ -1,17 +1,20 @@
-extends Node3D
+extends MeshInstance3D
 
-@onready var sphereMaterial: Material = $Sphere.mesh.surface_get_material(0)
 @export var light_position: MeshInstance3D
 
-var radiant_flux: float = 3.0 #Default 3.0
-var kd: Vector3 = Vector3(0.6, 0.3, 0.8) # Default (0.6, 0.3, 0.8)
-var ks: Vector3 = Vector3(1.0, 1.0, 1.0) # Default (1.0, 1.0, 1.0)
+var radiant_flux: float = 30.0 #Default 3.0
+var kd: Vector3 = Vector3(0.25, 0.5, 0.2) # Default (0.25, 0.5, 0.2)
+var ks: Vector3 = Vector3(0.25, 0.25, 0.25) # Default (1.0, 1.0, 1.0)
 var ns: float = 32.0 # Default 32.0
 
+var material: Material
 
 func _ready() -> void:
-	sphereMaterial.set_shader_parameter("radiant_flux", radiant_flux)
-	sphereMaterial.set_shader_parameter("kd", kd)
-	sphereMaterial.set_shader_parameter("ks", ks)
-	sphereMaterial.set_shader_parameter("ns", ns)
-	sphereMaterial.set_shader_parameter("light_position", light_position.position)
+	material = get_active_material(0)
+	material.set_shader_parameter("radiant_flux", radiant_flux)
+	material.set_shader_parameter("kd", kd)
+	material.set_shader_parameter("ks", ks)
+	material.set_shader_parameter("ns", ns)
+	material.set_shader_parameter("light_position", light_position.position)
+func _process(delta: float) -> void:
+	material.set_shader_parameter("light_position", light_position.position)
